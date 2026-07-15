0858fb41ad98972652ea0ee31227cd4bf46f1ec3
Alok <alokhotta10@gmail.com>
2026-07-15 20:09:26 +0530

    feat: implement workspace page with authenticated route and layout scaffolding

--- app/(main)/workspace/page.tsx
+++ app/(main)/workspace/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import WorkspaceClient from "@/components/WorkspaceClient";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const WorkspacePage = () => {
+interface WorkspacePageProps {
+    searchParams: Promise<{ prompt?: string; id?: string }>;
+}
+
+const WorkspacePage = async ({ searchParams }: WorkspacePageProps) => {
+    const user = await auth()
+    if(!user){
+        redirect("/")
+    }
+    const { prompt, id } = await searchParams
+
+    // const user = await getWorkspaceById() 
+
+    // let workspace = null;
+    // if (id) {
+    //     workspace = await getWorkspaceById(id, user.id);
+    // }
+
+
     return (
-        <div>Workspace Page</div>
+        <WorkspaceClient />
     )
 }
 
